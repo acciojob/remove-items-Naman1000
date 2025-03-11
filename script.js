ce730b804e9ec79aad02056be640e2e719d20cca
Naman1000 <91007344+Naman1000@users.noreply.github.com>
2025-03-11 18:47:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+//your JS code here. If
+function myFunction() {
+  const selectElement = document.getElementById("colorSelect");
+   const selectedIndex = selectElement.selectedIndex;
+
+    if (selectedIndex !== -1) {
+        selectElement.remove(selectedIndex);
+    }
+}
